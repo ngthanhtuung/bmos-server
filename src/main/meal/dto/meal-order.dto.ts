6e59d174bd3e1216a0bb28623236a562ab24dea1
ngthanhtuung <tungnt16092001@gmail.com>
2023-07-15 15:12:39 +0700

    Fix MealOrderDto

---
+++ src/main/meal/dto/meal-order.dto.ts
@@ -0,0 +1,11 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+
+export class MealOrderDto {
+
+    @ApiProperty()
+    public id: string;
+    @ApiProperty()
+    public amount: number;
+    
+}
